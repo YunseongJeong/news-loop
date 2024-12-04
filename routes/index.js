de2b2e2c804d6385718f8734eb9a5a2e003af092
mandu <jys0972@gmail.com>
2024-12-04 21:28:28 +0900

    feat(routes/index.js) :  router controller로 연결

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const {renderIndex} = require('../controllers/index');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NEWS LOOP' });
-});
+router.get('/', renderIndex);
 
 module.exports = router;
