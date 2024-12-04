21fd805dfe1e4a3d505587e238ea619d3ea8e43b
mandu <jys0972@gmail.com>
2024-12-04 21:28:51 +0900

    feat(routes/users.js) : router controller로 연결

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const {renderLogin} = require('../controllers/users');
 
 /* GET users listing. */
-router.get('/login', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/login', renderLogin);
 
 module.exports = router;
