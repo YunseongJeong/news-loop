440ce4ce45fcf2747097fe4005ddc6c8b7baad61
mandu <jys0972@gmail.com>
2024-12-05 20:47:01 +0900

    feat(routes/articles.js) : 댓글 관련 router 추가

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const {isLoggedIn} = require('../midwares');
 
-const {renderArticle} = require('../controllers/articles');
+const {renderArticle, saveComment, deleteComment} = require('../controllers/articles');
 
 router.get('/:aid', renderArticle);
-
+router.post('/comments/:aid', isLoggedIn, saveComment);
+router.delete('/comments/:cid', isLoggedIn, deleteComment);
 module.exports = router;
