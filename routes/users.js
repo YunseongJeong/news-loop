71950df996f59d686e18b5588a878f3e652c8389
mandu <jys0972@gmail.com>
2024-12-05 14:18:04 +0900

    feat(routes/users.js) : 로그인, 회원가입 기능 라우터 추가

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const {renderLogin} = require('../controllers/users');
+const {renderLogin, login, renderSignup, signup} = require('../controllers/users');
+const {isNotLoggedIn} = require('../midwares');
 
-/* GET users listing. */
-router.get('/login', renderLogin);
+// login page
+router.get('/login', isNotLoggedIn, renderLogin);
+router.post('/login', login);
+
+// sign up page
+router.get('/signup', renderSignup);
+router.post('/signup', signup);
 
 module.exports = router;
