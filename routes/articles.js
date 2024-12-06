354964d2f87d9d56bf9f7d70df318fa91306e98b
mandu <jys0972@gmail.com>
2024-12-06 15:41:33 +0900

    feat(routes/articles.js) : article 삭제 라우터 추가

--- routes/articles.js
+++ routes/articles.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 const {isLoggedIn} = require('../midwares');
 
-const {renderArticle, saveComment, deleteComment} = require('../controllers/articles');
+const {renderArticle, saveComment, deleteComment, deleteArticle} = require('../controllers/articles');
 
+router.delete('/:aid', isLoggedIn, deleteArticle);
 router.get('/:aid', renderArticle);
 router.post('/comments/:aid', isLoggedIn, saveComment);
 router.delete('/comments/:cid', isLoggedIn, deleteComment);
