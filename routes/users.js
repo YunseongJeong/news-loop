6f97836e9a02b5928bafa0f66451f542772152fe
mandu <jys0972@gmail.com>
2024-12-06 13:56:42 +0900

    feat(routes/users.js) : dashboard 관련 라우터 추가

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const {renderLogin, login, renderSignup, signup, logout} = require('../controllers/users');
+const {renderLogin, login, renderSignup, signup, logout, renderDashboard} = require('../controllers/users');
 const {isNotLoggedIn, isLoggedIn} = require('../midwares');
+
+router.get('/dashboard', isLoggedIn, renderDashboard);
 
 router.get('/logout', isLoggedIn, logout);
 
