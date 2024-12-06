0fd83ec631760c5934518d07609034ce4177b1a1
mandu <jys0972@gmail.com>
2024-12-06 19:01:56 +0900

    feat(articles.js) : articles 작성 관련 라우터 추가, multer 연결

--- routes/articles.js
+++ routes/articles.js
@@ -2,7 +2,15 @@ var express = require('express');
 var router = express.Router();
 const {isLoggedIn} = require('../midwares');
 
-const {renderArticle, saveComment, deleteComment, deleteArticle, like, unlike} = require('../controllers/articles');
+const {renderArticle, saveComment, deleteComment, deleteArticle, like, unlike, createArticle, renderEditArticle} = require('../controllers/articles');
+
+const multer = require('../services/multer');
+const upload = multer(); // for image
+
+
+router.get('/edit', isLoggedIn, renderEditArticle);
+router.post('/edit', isLoggedIn, upload.single('image_path'), createArticle);
+
 
 router.delete('/:aid', isLoggedIn, deleteArticle);
 router.get('/:aid', renderArticle);
