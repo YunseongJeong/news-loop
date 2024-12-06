0b0466ae2774e0ff464c516d80ff58c380d4dfdb
mandu <jys0972@gmail.com>
2024-12-06 16:45:35 +0900

    feat(routes/articles.js) : like, unlike 라우터 연결

--- routes/articles.js
+++ routes/articles.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 const {isLoggedIn} = require('../midwares');
 
-const {renderArticle, saveComment, deleteComment, deleteArticle} = require('../controllers/articles');
+const {renderArticle, saveComment, deleteComment, deleteArticle, like, unlike} = require('../controllers/articles');
 
 router.delete('/:aid', isLoggedIn, deleteArticle);
 router.get('/:aid', renderArticle);
 router.post('/comments/:aid', isLoggedIn, saveComment);
 router.delete('/comments/:cid', isLoggedIn, deleteComment);
+
+router.get('/like/:aid', isLoggedIn, like);
+router.get('/unlike/:aid', isLoggedIn, unlike);
+
 module.exports = router;
