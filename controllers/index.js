fb291f75cad918777e6aad43b3ec723e7c846684
mandu <jys0972@gmail.com>
2024-12-06 13:54:59 +0900

    feat(controllers/index.js) : articles 정보에 created_at 추가

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const db = require(process.cwd() + '/models');
 
 exports.renderIndex = async (req, res, next)=>{
     try{
-        const [articles] = await db.execute('select id, subject, description, image_path from articles');
+        const [articles] = await db.execute('select id, created_at, subject, description, image_path from articles');
         res.render('index', {articles, title: 'NEWS LOOP', isLoggedIn: req.isAuthenticated()});
     } catch (err){
         console.error(err);
