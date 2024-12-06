e7b38ed580e62744df0f0ed7eed4934740ee73e6
mandu <jys0972@gmail.com>
2024-12-06 19:00:38 +0900

    feat(controllers/index.js) : 최신순 정렬하도록 수정

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const db = require(process.cwd() + '/models');
 
 exports.renderIndex = async (req, res, next)=>{
     try{
-        const [articles] = await db.execute('select articles.id as id, subject, image_path, content, articles.created_at as created_at, username, ifnull(like_count, 0) as like_count from (articles join users on articles.uid=users.id) left join (select aid, count(1) as like_count from likes group by (aid)) as l on articles.id = l.aid;');
+        const [articles] = await db.execute('select articles.id as id, subject, image_path, content, articles.created_at as created_at, username, ifnull(like_count, 0) as like_count from (articles join users on articles.uid=users.id) left join (select aid, count(1) as like_count from likes group by (aid)) as l on articles.id = l.aid order by created_at desc;');
         res.render('index', {articles, title: 'NEWS LOOP', isLoggedIn: req.isAuthenticated()});
     } catch (err){
         console.error(err);
