713be7c975cd73a19cba849ff59380c9f71a41f4
mandu <jys0972@gmail.com>
2024-12-06 16:44:15 +0900

    feat(controllers/index.js): like_count까지  조회하도록 sql 수정

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const db = require(process.cwd() + '/models');
 
 exports.renderIndex = async (req, res, next)=>{
     try{
-        const [articles] = await db.execute('select id, created_at, subject, description, image_path from articles');
+        const [articles] = await db.execute('select articles.id as id, subject, image_path, content, articles.created_at as created_at, username, ifnull(like_count, 0) as like_count from (articles join users on articles.uid=users.id) left join (select aid, count(1) as like_count from likes group by (aid)) as l on articles.id = l.aid;');
         res.render('index', {articles, title: 'NEWS LOOP', isLoggedIn: req.isAuthenticated()});
     } catch (err){
         console.error(err);
