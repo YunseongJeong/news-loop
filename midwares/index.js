f893a2d7685f6dc74f47b5eaa1f6e42812c7d7ad
mandu <jys0972@gmail.com>
2024-12-06 13:28:41 +0900

    feat(midewares/index.js) : 로그인 필요 이벤트 더욱 구체적으로 구현

--- midwares/index.js
+++ midwares/index.js
@@ -2,7 +2,7 @@ exports.isLoggedIn = (req, res, next) =>{
     if (req.isAuthenticated()){
         next();
     } else {
-        res.status(403).send('로그인 필요');
+        res.status(403).render('error', { error: {status: 403}, message: '로그인 필요'});
     }
 };
 
